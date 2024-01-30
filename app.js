38cf53cbe4442a47a8472a1c60ad4c4a49243420
Ortyom <artembitsa6@gmail.com>
2024-01-30 18:40:13 +0300

    Made footer and base backend

---
+++ app.js
@@ -0,0 +1,28 @@
+const { publicDecrypt } = require("crypto");
+const express = require("express");
+const fs = require("fs");
+const app = express();
+const PORT = 3000;
+
+// EJS
+app.set("view engine", "ejs");
+
+//Статичная папка
+app.use(express.static("public"));
+
+// Преобразование данных из буфера в читаемый вид
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+
+
+// Рендер главной страницы
+app.get(["/", "/index"], (req, res) => {
+	res.render("index", { activePage: "index" });
+});
+
+
+
+app.listen(PORT, () => {
+	console.log("см. тут: http://localhost:" + PORT);
+});
